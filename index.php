2e0ef003cf7d3c95ff540fab6ef169fe597cfa0d
Ronaldtbl <63965321+Ronaldtbl@users.noreply.github.com>
2020-04-21 17:56:01 +0600

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $method = $_SERVER['REQUEST_METHOD'];
 if($method == 'POST'){
 	$requestBody = file_get_contents('php://input');
 	$json = json_decode($requestBody);
-
+	echo var_dump($json) . "<br>";
 	//$text = $json->result->parameters->text;
 	$text = $json->queryResult->queryText;
 
